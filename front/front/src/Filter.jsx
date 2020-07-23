ab90d8eebaf951f230875e701931d8ee28a96d5f
Justine810 <justine.gautreau@gmail.com>
2020-07-23 17:20:39 +0200

    style select

--- front/front/src/Filter.jsx
+++ front/front/src/Filter.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 //import "./styles.css";
 import TripList from "./TripList";
 import axios from "axios";
+import './select.scss';
 
 const continents = ["Filtrer par continent", "Afrique", "Amérique du Nord", "Amérique du Sud", "Asie", "Europe", "Océanie"];
 
@@ -34,7 +35,7 @@ export default class Filter extends Component {
     const filteredTrips = articles.filter(article => !filter || article.continent_name === filter);
       return (
       <div className="App">
-        <select onChange={this.handleFilterChange}>
+        <select className="select-css" onChange={this.handleFilterChange}>
           {continents.map(continent => (
             <option value={continent}>{continent}</option>
           ))}
